9d681df135591079a77decc6a7f6c76a2bfc8be0
Firuz Khamroev <fkhamroev@icloud.com>
2024-01-06 14:48:44 +0500

    test vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://fkhamroev.github.io/drag-drop/",
   plugins: [react()],
-  base: '/drag-drop/'
-})
+});
